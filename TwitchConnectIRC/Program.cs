99c28f1af07cc32c421af336ea6038fe5930eb2e
ArgentumVir <argentumvir@gmail.com>
2016-05-19 16:08:43 +0900

    Removed my oauth key I'm smart I swear

--- TwitchConnectIRC/Program.cs
+++ TwitchConnectIRC/Program.cs
@@ -11,7 +11,7 @@ namespace TwitchConnectIRC
 	    static void Main(string[] args)
 	    {
             // Jooj plz dont steal my oauth code :(
-            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "argentumvir", "oauth:fsxdrod6u373j4q5t0yt12ciy6pf2x");
+            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "argentumvir", "");
 
             irc.joinRoom("argentumvir");
             while (true)
