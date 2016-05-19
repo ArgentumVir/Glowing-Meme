9514ba0bf45eb673b2f2951ff5132ac139d23f62
ArgentumVir <argentumvir@gmail.com>
2016-05-19 16:03:13 +0900

    Init IRC

---
+++ TwitchConnectIRC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchConnectIRC
+{
+    public class Program
+    {
+	    static void Main(string[] args)
+	    {
+            // Jooj plz dont steal my oauth code :(
+            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "argentumvir", "oauth:fsxdrod6u373j4q5t0yt12ciy6pf2x");
+
+            irc.joinRoom("argentumvir");
+            while (true)
+            {
+                string message = irc.readMessage();
+                Console.WriteLine(message);
+            }
+	    }
+    }
+}
