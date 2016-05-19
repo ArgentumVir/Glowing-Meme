222c5a6e7a82617085ec23065be8b1c507d7e10a
ArgentumVir <argentumvir@gmail.com>
2016-05-19 16:48:09 +0900

    Added Config Handling

--- TwitchConnectIRC/Program.cs
+++ TwitchConnectIRC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,42 @@ namespace TwitchConnectIRC
 {
     public class Program
     {
+        static private string username;
+        static private string oauth;
+        private static string configFile = "../../IRC_CONFIG.txt";
+
 	    static void Main(string[] args)
 	    {
-            // Jooj plz dont steal my oauth code :(
-            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "argentumvir", "");
+            
+            setCredentials();
+            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, username, oauth);
 
-            irc.joinRoom("argentumvir");
+            irc.joinRoom(username);
             while (true)
             {
                 string message = irc.readMessage();
                 Console.WriteLine(message);
             }
+
+            Console.ReadKey();
 	    }
+
+        private static void setCredentials()
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader(configFile))
+                {
+                    username = sr.ReadLine();
+                    oauth = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to read config file you're fucked. ");
+                Console.WriteLine(e.Message);
+            }
+
+        }
     }
 }
